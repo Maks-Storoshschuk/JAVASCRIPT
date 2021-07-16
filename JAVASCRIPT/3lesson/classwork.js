10a0b2b2a24fc0ab59ac9a36a32b954e082982e9
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-07-16 16:18:19 +0300

    додав в один  масив

--- JAVASCRIPT/3lesson/classwork.js
+++ JAVASCRIPT/3lesson/classwork.js
@@ -4,15 +4,31 @@ function power(a,b,c){
 let massiv = power(0,"c==3",":D")
 console.log(massiv)
 
-function tripple(a,b,c){
-    return push a+b+c;
-}
-    let numbers =tripple(123,234,345);
-console.log(numbers)
 
-for (let number of numbers){
-    if ( number <= tripple){
-        console.log(number)
+
+
+function minArr(num1,num2,num3) {
+    let minNum = num1
+    if (minNum > num2) {
+        minNum=num2
     }
+    else if(minNum>num3){
+        minNum=num3
+    }
+    console.log(minNum)
 }
+minArr(123,234,345)
 
+
+function maxArr(num1,num2,num3) {
+    let maxNum = num1
+    if (maxNum > num2) {
+        maxNum=num1
+    }
+    else if(maxNum<num3){
+        maxNum=num3
+    }
+    console.log(maxNum)
+
+}
+maxArr(123,234,345)
