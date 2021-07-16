e6f457c70ccb1604cac47904a8c52f16a42ff507
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-07-16 19:30:00 +0300

    починаю розуміти їбучий синтаксис

--- JAVASCRIPT/3lesson/classwork.js
+++ JAVASCRIPT/3lesson/classwork.js
@@ -29,6 +29,47 @@ function maxArr(num1,num2,num3) {
         maxNum=num3
     }
     console.log(maxNum)
-
 }
 maxArr(123,234,345)
+
+
+function max (array){
+    let max = array[0];
+    for (const maxItem of array) {
+        if (maxItem > max) max = maxItem;
+    }
+    return max;
+}
+let maxMas = max([123,234,456,567,678]);
+console.log(maxMas);
+
+function min (array){
+    let min = array[0]
+    for (let minItem of array){
+        if (minItem < min) min = minItem;
+    }
+    return min;
+}
+let minMas = min([123,234,456,567,678]);
+console.log(minMas);
+
+function suma (arr){
+    let suma = 0;
+    for (let sumaElement of arr) {
+   suma=suma+sumaElement;
+    }
+    return suma;
+}
+let sum = suma([123,234,345])
+console.log(sum)
+
+function aver (arr2){
+    let aver = 0;
+    for (let i = 0; i < arr2.length; i++) {
+        const arr2Element = arr2[i];
+        aver=aver+arr2Element/arr2.length
+    }
+    return aver;
+}
+let average = aver([123,234,345])
+console.log(average)
