56f6161779e03da17c618a59e66b42bb539bac55
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-07-29 14:21:44 +0300

    pff

--- JAVASCRIPT/7lesson/promise.js
+++ JAVASCRIPT/7lesson/promise.js
@@ -1,38 +1,48 @@
-
+function aweting(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('1) Прокинутись');
+        },1000)
+    })
+}
+function diner(){
+    setTimeout(()=>{
+        console.log('2) Поснідати');
+    },500)
+}
+function wash(){
+    setTimeout(()=>{
+        console.log('3) Піти в душ');
+    },2000)
+}
 function repeat(){
     setTimeout(()=>{
         console.log('4) Повторити лекцію');
-        bag();
     },300)
 }
 function bag(){
     setTimeout(()=>{
         console.log('5) Скласти рюкзак');
-        goOut();
     },100)
 }
 function goOut(){
     setTimeout(()=>{
         console.log('6) Йти на трамвай');
-        listen();
     },4000)
 }
 function listen(){
     setTimeout(()=>{
         console.log('7) Слухати лекцію');
-        practic();
     },2000)
 }
 function practic(){
     setTimeout(()=>{
         console.log('8) Практикувати');
-        goToHome();
     },400)
 }
 function goToHome(){
     setTimeout(()=>{
         console.log('9) Їхати додому');
-        fuckWife();
     },800)
 }
 function fuckWife(){
@@ -40,24 +50,7 @@ function fuckWife(){
         console.log('10) Трахати красуню дружину =D')
     })
 }
-function aweting(){
-    setTimeout(()=>{
-        console.log('1) Прокинутись');
-        diner();
-    },1000)
-}
-function diner(){
-    setTimeout(()=>{
-        console.log('2) Поснідати');
-        wash();
-    },500)
-}
-function wash(){
-    setTimeout(()=>{
-        console.log('3) Піти в душ');
-        repeat();
-    },2000)
-}
+
 let baton = document.getElementById('start')
 baton.onclick = function (){
     aweting()
